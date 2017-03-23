b5f0db5d00eb2e901a044b1703904d1d0da4f568
Sudhikar Kaki <skaki@skaki-MacBook.local>
2017-03-23 14:23:26 -0700

    bot

---
+++ app.js
@@ -0,0 +1,31 @@
+var restify = require('restify');
+var builder = require('botbuilder');
+
+//=========================================================
+// Bot Setup
+//=========================================================
+
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+   console.log('%s listening to %s', server.name, server.url); 
+});
+  
+// Create chat bot
+var connector = new builder.ChatConnector({
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
+});
+var bot = new builder.UniversalBot(connector);
+server.post('/api/messages', connector.listen());
+
+//=========================================================
+// Bots Dialogs
+//=========================================================
+
+bot.dialog('/', function (session) {
+    session.send("Hello World");
+});
+
+// AppID: 24513be5-f326-48e0-b5c8-8f4a9de96b38
+// App Password : p1ZHb0GnS5pLiO8Bo8tkp96
